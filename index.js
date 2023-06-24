43f7deb4f256f9a34c6b7f63fcc30371cb305316
mg <amg.vergara@gmail.com>
2023-06-23 23:04:25 -0400

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-   const express = require('express');
-   const { Pool } = require('pg');
-   require('dotenv').config()
+const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
 
-   const app = express();
-   const port = 3000;
+const app = express();
+const port = 3000;
 
 //    // Configura la conexión a la base de datos PostgreSQL
 //    const pool = new Pool({
@@ -14,25 +14,31 @@
 //      port: 5432,
 //    });
 
-
-
-
-
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+});
 
-   // Ruta de ejemplo
-   app.get('/', (req, res) => {
-     pool.query('SELECT NOW()', (error, results) => {
-       if (error) {
-         throw error;
-       }
-       res.send(`La fecha y hora actual es: ${results.rows[0].now}`);
-     });
-   });
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    preflightContinue: true,
+  })
+);
 
-   // Inicia el servidor
-   app.listen(port, () => {
-     console.log(`Servidor Express escuchando en el puerto ${port}`);
-   });
+// Ruta de ejemplo
+app.get("/", (req, res) => {
+  pool.query("SELECT NOW()", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.send(`La fecha y hora actual es: ${results.rows[0].now}`);
+  });
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
